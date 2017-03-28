e79507f273264525979d6a4e5b13029131b7bce1
madhankumar028 <madhankumar028@gmail.com>
2017-03-28 15:00:17 +0530

    added negative scenarios for stack implementation

--- datastructure/script/stack.js
+++ datastructure/script/stack.js
@@ -1,10 +1,16 @@
+var maxSize = 10,
+    minSize = 0;
+
 export function push(array, element) {
     
     var isStackFull = checkForOverflow(array),
         top = array.length;
 
-    if (isStackFull)
-        array[top] = element;
+    if (isStackFull) {
+        array[top] = element;        
+    } else {
+        return 'The stack is overflow';
+    }
         
     return array;
 }
@@ -13,6 +19,9 @@ export function pop(array, element) {
 
     var isStackEmpty = checkForUnderflow(array),
         top = array.length;
+    
+    if (element !== top - 1)
+        return 'Target element is not in the top of the stack';
 
     if (isStackEmpty)
         array.splice(top - 1, 1);
@@ -21,9 +30,9 @@ export function pop(array, element) {
 }
 
 function checkForOverflow(array) {
-    return array.length > 10 ? false : true;
+    return array.length > maxSize ? false : true;
 }
 
 function checkForUnderflow(array) {
-    return array.length === 0 ? false : true;
+    return array.length > minSize ? false : true;
 }
--- test/stack.test.js
+++ test/stack.test.js
@@ -9,14 +9,29 @@ describe('Stack test suite', function() {
         
         var newArray = stack.push(array, 10);
         
-        console.log(newArray);
+        chai.assert.sameMembers(array, [1, 4, 5, 7, 10],
+                    `new element isn't inserted into the stack`);        
+    });
+
+    it('Stack overflow test (failurecase)', function() {
+        
+        var array = [1, 2, 3, 4, 5, 6 ,7, 8, 9, 0, 10],
+            newArray;
+
+        newArray = stack.push(array, 12);
+        
+        chai.assert.sameMembers(array, newArray, 'the stackoverflow push not allowed');
     });
 
     it('Popping out an element from a stack', function() {
-
         var newArray = stack.pop(array, 10);
-
-        console.log(newArray);
+        chai.assert([1, 4, 5, 7], newArray, 'the target is not removed from the stack');
     });
+
+    it('Popping out mid element from the stack (failure case)', function() {        
+        var newArray = stack.pop(array, 4);
+        chai.assert.sameMembers([1, 5, 7, 10], newArray,
+                    'The target element is not top of the stack');       
+    })
 });
     
