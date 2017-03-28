f2b6ce968555cd5efb724b06ae43c070f7413ad7
madhankumar028 <madhankumar028@gmail.com>
2017-03-28 15:28:58 +0530

    fixed negative scenario for stack implementation

--- datastructure/script/stack.js
+++ datastructure/script/stack.js
@@ -1,38 +1,36 @@
 var maxSize = 10,
     minSize = 0;
 
-export function push(array, element) {
-    
-    var isStackFull = checkForOverflow(array),
-        top = array.length;
+export function push(stack, element) {   
+    var isStackFull = checkForOverflow(stack),
+        top = stack.length;
 
     if (isStackFull) {
-        array[top] = element;        
+        stack[top] = element;        
     } else {
         return 'The stack is overflow';
     }
         
-    return array;
+    return stack;
 }
 
-export function pop(array, element) {
+export function pop(stack, element) {
+    var isStackEmpty = checkForUnderflow(stack),
+        top = stack.length;    
 
-    var isStackEmpty = checkForUnderflow(array),
-        top = array.length;
-    
-    if (element !== top - 1)
-        return 'Target element is not in the top of the stack';
+    if (isStackEmpty) {
+        stack.splice(top - 1, 1);        
+    } else {
+        return 'the stack is empty';
+    }
 
-    if (isStackEmpty)
-        array.splice(top - 1, 1);
-
-    return array;
+    return stack;
 }
 
-function checkForOverflow(array) {
-    return array.length > maxSize ? false : true;
+function checkForOverflow(stack) {
+    return stack.length > maxSize ? false : true;
 }
 
-function checkForUnderflow(array) {
-    return array.length > minSize ? false : true;
+function checkForUnderflow(stack) {    
+    return stack.length === minSize ? false : true;
 }
--- test/stack.test.js
+++ test/stack.test.js
@@ -3,35 +3,34 @@ import * as stack from '../datastructure/script/stack';
 
 describe('Stack test suite', function() {
 
-    var array = [1, 4, 5, 7];
+    var stackArray = [1, 4, 5, 7];
 
-    it('Pushing an element into stack', function() {
-        
-        var newArray = stack.push(array, 10);
-        
-        chai.assert.sameMembers(array, [1, 4, 5, 7, 10],
+    it('Pusing into stack', function() {
+        var newArray = stack.push(stackArray, 10);
+        chai.assert.sameMembers(stackArray, newArray,
                     `new element isn't inserted into the stack`);        
     });
 
-    it('Stack overflow test (failurecase)', function() {
-        
-        var array = [1, 2, 3, 4, 5, 6 ,7, 8, 9, 0, 10],
+    it('Pushing into overflowed stack', function() {        
+        var overflowArray = [1, 2, 3, 4, 5, 6 ,7, 8, 9, 0, 10],
             newArray;
 
-        newArray = stack.push(array, 12);
+        newArray = stack.push(overflowArray, 12);
         
-        chai.assert.sameMembers(array, newArray, 'the stackoverflow push not allowed');
+        chai.assert.sameMembers(overflowArray, newArray, 'the stackoverflow push not allowed');
     });
 
-    it('Popping out an element from a stack', function() {
-        var newArray = stack.pop(array, 10);
+    it('Popping out from stack', function() {
+        var newArray = stack.pop(stackArray, 10);
         chai.assert([1, 4, 5, 7], newArray, 'the target is not removed from the stack');
     });
 
-    it('Popping out mid element from the stack (failure case)', function() {        
-        var newArray = stack.pop(array, 4);
-        chai.assert.sameMembers([1, 5, 7, 10], newArray,
-                    'The target element is not top of the stack');       
-    })
+    it('Popping out from empty stack', function() {
+        var underflowArray = [],
+            newArray;
+
+        newArray = stack.pop(underflowArray, 2);
+        chai.assert.sameMembers(underflowArray, newArray, 'the stack is empty');
+    });
 });
     
