b8b495d52361c7960953fafd4ed4f1d77422c56e
madhankumar028 <madhankumar028@gmail.com>
2017-04-04 14:47:05 +0530

    initial commit for watertower problwm

--- toyproblem/waterTower.js
+++ toyproblem/waterTower.js
@@ -3,9 +3,31 @@
  *
  * Author Madhankumar<madhankumar028@gmail.com>
  * 
- * Input: [1,5,3,7,2] (width of each tower is 1 unit), Output: 2 units
+ * Input: [1,0,3,2] (width of each tower is 1 unit), Output: 1 units
  * 
  */
-export function calculateUnits() {
+export function calculateWaterUnits(heights) {
     
+    var isTowerSorted;
+    
+    heights.forEach(function(tower) {
+        isTowerSorted = isSorted(tower, heights);
+    });
+
+    if (isTowerSorted)
+        return 'water is not collected';
+    
+    calculateTowersDifference(heights);
+}
+
+function isSorted(element, heights) {    
+
+}
+
+function calculateTowersDifference(heights) {
+    for ( let i = 0; i < heights.length; i++ ) {
+        for ( let j = 1; j < heights.length; j++ ) {
+            
+        }
+    }
 }
